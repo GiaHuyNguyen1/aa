43006d59a68372775a10326f6c493eafb01f1eb8
GiaHuy1 <antongiahuy@gmail.com>
2021-06-18 09:03:08 +0700

    Add HomePage

--- App1/App1/App1/AppShell.xaml.cs
+++ App1/App1/App1/AppShell.xaml.cs
@@ -11,6 +11,7 @@ namespace App1
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(HomePage), typeof(HomePage));
             Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
         }
